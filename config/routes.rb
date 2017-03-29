0ff516a3f3882161393b553e483f34242a5de2a5
tgt87 <golduck21187@yahoo.com.sg>
2017-03-29 23:54:42 +0800

    able to hire

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   devise_for  :users, :controllers => {:registrations => "users/registrations"}
   resources :jobs
-  resources :job_applications
+  resources :job_applications do
+    get 'update_status'
+  end
   # devise_for :employers, :controllers => {:registrations => "users/registrations"}
   # devise_for :employees
   resources :employers
