5178ccb72a0d7eead2db5b61cb2b382cd52335f2
elaine lim <lainex@gmail.com>
2017-04-04 15:53:54 +0800

    stars are working

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -2,8 +2,8 @@ document.addEventListener("turbolinks:load", function() {
   $("#star-rating").empty()
   var num = $('#rating').html()
   var star = "<span></span>"
+
   for(i = 0; i< num; i++){
     $("#star-rating").append(star)
   }
-
 })
