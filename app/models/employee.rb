73795112adfebd87c23842617627080aa1f84a3b
Shimei Wong <wshimei@gmail.com>
2017-04-05 10:05:37 +0800

    added image to employee/er

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,5 @@ class Employee < ApplicationRecord
   has_one :user
   has_many :job_applications
   has_many :jobs, through: :job_applications
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403085944) do
+ActiveRecord::Schema.define(version: 20170405020419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170403085944) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "employers", force: :cascade do |t|
@@ -45,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170403085944) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "job_applications", force: :cascade do |t|
@@ -97,7 +99,6 @@ ActiveRecord::Schema.define(version: 20170403085944) do
     t.string   "role"
     t.integer  "employer_id"
     t.integer  "employee_id"
-    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
