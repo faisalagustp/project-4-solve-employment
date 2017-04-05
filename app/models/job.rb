fb0148e5946ca9664f5d725ab46be4c5d83f5a6c
tgt87 <golduck21187@yahoo.com.sg>
2017-04-05 20:30:32 +0800

    added vadilations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,13 @@ class Job < ApplicationRecord
   belongs_to :employer
   has_many :job_applications, dependent: :destroy
   has_many :employees, through: :job_applications
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :wage, numericality: { only_integer: true }
+  validates :contact_person, presence: true
+  validates :contact_email, presence: true
+  validates :job_type, presence: true
+  validates :time_commitment, numericality: true
+  validates :location, presence: true
+  validates :positions, numericality: { only_integer: true }
 end
